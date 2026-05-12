c2bc08b21e9cd364f0aeb36df41775dc98deb8e9
sisu-01 <tesv010615@naver.com>
2026-05-12 16:48:30 +0900

    언젠가 프로필

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,8 +1,10 @@
+import styles from "./Profile.module.css";
+
 const Profile = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <h1>프로필사진</h1>
-      <h2>이름 및 자기소개 어쩌구 저쩌구</h2>
+      <h2>이름 및 자기소개</h2>
     </div>
   );
 }
