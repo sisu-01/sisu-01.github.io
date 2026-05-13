fe0fcad9813f8be884b7ffc46254cc4df39d1492
sisu-01 <tesv010615@naver.com>
2026-05-13 17:18:12 +0900

    강조

--- src/components/TechStack/TechStack.tsx
+++ src/components/TechStack/TechStack.tsx
@@ -9,12 +9,16 @@ interface TechStackProps {
 }
 
 const TechStack = ({ techStack, getCategories }: TechStackProps) => {
+  const isAnySelected = getCategories.length > 0;
   return (
     <ul className={styles.container}>
       {techStack.map((stack) => {
-        const isActive = getCategories.includes(stack.type as Type);
+        // 2. 강조 로직 수정: 
+        // - 아무것도 선택되지 않았으면 모두 활성(true)
+        // - 무언가 선택되었다면, 현재 스택이 포함되어 있는지 확인
+        const isActive = !isAnySelected || getCategories.includes(stack.type as Type);
         return (
-          <li key={stack.type} className={`${stack.type} ${isActive ? styles.active : ''}`}>
+          <li key={stack.type} className={`${styles.stackSection} ${isActive ? styles.active : styles.inactive}`}>
             <div className={styles.category} style={{ borderLeft: `var(--color-${stack.type}) 5px solid` }}>{stack.type}</div>
             <ul className={styles.container}>
               {stack.contents.map((content) => (
