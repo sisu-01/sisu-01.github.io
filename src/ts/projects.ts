47d481d8979f8f8724ba4a66919e3e2124e671d4
sisu-01 <tesv010615@naver.com>
2026-06-10 17:12:45 +0900

    에으

--- src/ts/projects.ts
+++ src/ts/projects.ts
@@ -6,6 +6,7 @@ export const PROJECTS: ProjectType[] = [
     title: "PUBG 박격포 계산기",
     subtitle: "박격포 사격제원 산출 자동화 프로그램",
     images: [
+      "/test/4.png",
       "/test/1.png",
       "/test/2.gif",
       "/test/3.png",
@@ -29,7 +30,7 @@ export const PROJECTS: ProjectType[] = [
         ]
       }
     ],
-    etc: "Ai의 도움을 많이 받은 프로젝트였다.\n" + 
+    etc: "Ai의 힘을 극한으로 발휘한 프로젝트였다.\n" + 
         "하지만 Ai는 결국 도구일 뿐, 개발의 방향성과 아이디어같은 설계 능력은 개발자의 몫임을 느꼈다.\n\n" +
         "Ai가 해준 것들\n" + 
         "1. PUBG 인게임 박격포 탄속 역산하기\n2. 수평 거리와 고도차를 고려한 탄도학 공식 만들기\n3. OpenCV 기능 도움",
@@ -245,7 +246,7 @@ export const PROJECTS: ProjectType[] = [
       }
     ],
     etc:
-    "회사 업무 외에, 개인적으로 기획·개발하여 실제 현장에 도입되고 돈을 번 프로젝트다.\n"+
+    "회사 업무 외에, 개인적으로 기획·개발하여 실제 현장에 도입되고 수익을 가져다준 프로젝트다.\n"+
     "현재까지도 써브웨이에서 내가 만든 애플레케이션이 사용되고 있을 것이다.\n\n"+
     "기술적으로 어렵지 않고 작은 프로젝트였지만, 위와 같은 이유로 메인 프로젝트로 분류해놨다.",
     repoUrl: "https://github.com/sisu-01/subway-label-printer",
@@ -253,13 +254,13 @@ export const PROJECTS: ProjectType[] = [
   },
   {
     title: "카연갤 북마크",
-    subtitle: "아마추어 웹툰 플랫폼",
+    subtitle: "만화 필터링·클러스터링 웹 서비스",
     images: [
       "/test/1.png",
       "/test/2.gif",
       "/test/3.png",
     ],
-    summary: "무분별한 만화들을 수집 및 가공하여 보기 편하게 만들고 시리즈화 하는 웹서비스",
+    summary: "디시인사이드 카툰-연재 갤러리의 게시글을 수집 및 가공하여 보기 편하게 만들고 손쉽게 공유 가능한 웹 서비스",
     icon: ["fas", "book-bookmark"],
     isMain: true,
     techStack: [
@@ -268,8 +269,9 @@ export const PROJECTS: ProjectType[] = [
         tags: [/*"HTML", "CSS", */"JavaScript", "React", "Next.js"],
         contents: [
           {
-            features: "SEO 최적화를 위한 React -> Next.js 마이그레이션",
-            description: "의도했던 SEO 및 공유 opengraph 기능을 React에서는 구현이 불간으하여 Next.js로 마이그레이션하여 구현했습니다."
+            features: "React -> Next.js 마이그레이션",
+            description: "SNS 공유 시 만화별 동적 OpenGraph 적용을 위해 CSR(React) 구조에서 SSR(Next.js) 기반으로 마이그레이션"
+            
           }
         ]
       },
@@ -278,12 +280,12 @@ export const PROJECTS: ProjectType[] = [
         tags: ["Python", "MongoDB"],
         contents: [
           {
-            features: "만화 데이터 수집 및 군집화",
-            description: "웹에서 만화 데이터를 수집하고, 만화 제목들을 전처리, 임베딩 및 군집화하여 흩어진 만화들을 시리즈로 묶어 사용자가 보기 편하게 만들었습니다."
+            features: "전처리 -> 벡터화 -> 군집화",
+            description: "수집한 만화 정보를 전처리(MeCab), 벡터화(TF-IDF) 및 군집화(DBSCAN) 하여 만화들을 시리즈별로 분류"
           },
           {
             features: "MongoDB를 활용한 데이터 저장",
-            description: "수집한 만화 데이터를 MongoDB에 저장하여 빠른 검색과 유연한 데이터 구조를 활용했습니다."
+            description: "다양한 경험을 위해 RDBMS에서 벗어나 NoSQL 경험 및 Mongo Atlas의 무료 플랜을 활용하여 DB 구축"
           }
         ]
       },
@@ -292,17 +294,20 @@ export const PROJECTS: ProjectType[] = [
         tags: ["Linux", "Docker", "Vercel"],
         contents: [
           {
-            features: "Docker를 활용한 배포 환경 구축",
-            description: "Docker 컨테이너를 사용하여 애플리케이션의 배포 환경을 일관되게 유지하고, 버전 관리 및 배포의 효율성을 높였습니다."
+            features: "Docker 배포 환경 구축",
+            description: "Docker 컨테이너를 활용하여 배포 편이성 향상 및 환경 일관성 유지"
           },
           {
-            features: "Vercel을 통한 프론트엔드 배포",
-            description: "Next.js 애플리케이션을 Vercel에 배포하여 빠르고 안정적인 호스팅 환경을 제공했습니다."
+            features: "Vercel 배포",
+            description: "GitHub 연동 자동 배포 설정을 통해 개발 편이성 향상 및 무료 호스팅 활용"
           }
         ]
       }
     ],
-    etc: "처음으로 JS 라이브러리 사용해봤는데 놀라운 편의성에 감탄했다.",
+    etc:
+    "처음으로 JS 라이브러리(React, Next.js)를 사용해봤는데 놀라운 개발 편의성에 감탄했다.\n내가 지금까지 사용했던 Django나 Spring과 비교해서 쉽고, 빠르고, 자유로운 개발 경험을 제공했다.\n\n"+
+    "현재 나의 군집화 알고리즘은 제목의 맥락을 이해하지 못해 전혀 다른 만화라도 비슷한 제목이면 같은 시리즈로 분류하는 버그가 있다.\n"+
+    "기회가 된다면 군집화 알고리즘을 개선하여 더 정확한 시리즈 분류 기능을 완성하고싶다.",
     repoUrl: "https://github.com/sisu-01/cartoon_nextjs",
     serviceUrl: "https://cartoon-nextjs.vercel.app/"
   },
@@ -316,7 +321,7 @@ export const PROJECTS: ProjectType[] = [
     ],
     summary: "요약입니두~",
     icon: ["fas", "newspaper"],
-    isMain: true,
+    isMain: false,
     techStack: [
       {
         type: "프론트엔드",
@@ -327,29 +332,31 @@ export const PROJECTS: ProjectType[] = [
         type: "백엔드",
         tags: ["Python", "Django", "MariaDB"],
         contents: [
-          {
-            features: "Django 프레임워크를 활용한 웹 애플리케이션 개발",
-            description: "Django의 MTV 아키텍처를 활용하여 모델, 템플릿, 뷰를 구성하여 개인 블로그 웹사이트를 개발했습니다."
-          }
+          // {
+          //   features: "Django 프레임워크를 활용한 웹 애플리케이션 개발",
+          //   description: "Django의 MTV 아키텍처를 활용하여 모델, 템플릿, 뷰를 구성하여 개인 블로그 웹사이트를 개발했습니다."
+          // }
         ]
       },
       {
         type: "인프라",
         tags: ["Linux", "Nginx", "Docker"],
         contents: [
-          {
-            features: "Docker를 활용한 배포 환경 구축",
-            description: "Docker 컨테이너를 사용하여 애플리케이션의 배포 환경을 일관되게 유지하고, 버전 관리 및 배포의 효율성을 높였습니다."
-          }
+          // {
+          //   features: "Docker를 활용한 배포 환경 구축",
+          //   description: "Docker 컨테이너를 사용하여 애플리케이션의 배포 환경을 일관되게 유지하고, 버전 관리 및 배포의 효율성을 높였습니다."
+          // }
         ]
       }
     ],
-    etc: "",
+    etc:
+    "Django는 여러 기능들이 기본으로 내장돼있고, 심지어 관리자 페이지까지 제공된다.\n"+
+    "또한 이전까지는 sql문을 직접 작성했지만 처음으로 orm을 사용해보면서 빠른 개발 속도를 경험했다.",
     repoUrl: "https://github.com/sisu-01/sisu2"
   },
   {
     title: "PowerWinup",
-    subtitle: "자산관리 및 윈도우 업데이트 제어",
+    subtitle: "대규모 에이전트 중앙 관리 솔루션",
     images: [
       "/test/1.png",
       "/test/2.gif",
@@ -365,7 +372,7 @@ export const PROJECTS: ProjectType[] = [
         contents: [
           {
             features: "관리자 페이지 대시보드 구현",
-            description: "chart-js 라이브러리를 활용하여 에이전트 현황 및 서버 상태를 시각화했습니다."
+            description: "chart-js 라이브러리를 활용하여 에이전트 현황 및 서버 상태를 시각화"
           }
         ]
       },
@@ -374,8 +381,8 @@ export const PROJECTS: ProjectType[] = [
         tags: ["Java", "Spring", "WebSocket", "MariaDB"],
         contents: [
           {
-            features: "WebSocket을 통한 실시간 데이터 전송",
-            description: "관리자 페이지(프론트)에서 버튼을 누르면 WebSocket을 통해 서버로 명령이 전송되고, 서버는 해당 명령을 에이전트로 전달하여 실시간으로 업데이트를 제어할 수 있도록 구현했습니다."
+            features: "WebSocket 활용 실시간 통신 구현",
+            description: "에이전트 개발자와 협업하여 실시간 통신, 명령어 전송 및 상태 업데이트 기능 구현"
           }
         ]
       },
@@ -385,12 +392,12 @@ export const PROJECTS: ProjectType[] = [
         contents: [
           {
             features: "네트워크 환경 구성",
-            description: "각 고객사의 유지보수 팀과 협업하여 저마다 다른 환경에서 발생하는 네트워크적 이슈를 해결하고 제품의 안정성을 높였습니다."
+            description: "고객사의 유지보수 팀과 협업하여 네트워크 오류 해결 및 안정성 향상"
           }
         ]
       }
     ],
-    etc: "GS 인증 획득 과정을 통해 피드백 및 개선을 반복하여 제품의 완성도를 높였습니다.",
+    etc: "GS 인증 획득 과정을 통해 피드백 및 개선을 반복하여 제품의 완성도를 높일 수 있었다.\n\n내가 만든 프로그램이 전국의 다양한 기업들에서 실제로 사용된다는 사실에 매우 기쁘고 큰 보람을 느꼈다.",
   },
   {
     title: "V-pad",
@@ -399,7 +406,6 @@ export const PROJECTS: ProjectType[] = [
       "/test/1.png",
       "/test/2.gif",
       "/test/3.png",
-      "/test/4.png",
     ],
     summary: "요약입니두~",
     icon: ["fas", "keyboard"],
@@ -410,7 +416,7 @@ export const PROJECTS: ProjectType[] = [
         tags: [/*"HTML", "CSS", */"JavaScript"],
         contents: [
           {
-            features: "키보드 대신 마우스로 입력하는 가상 키패드 구현",
+            features: "가상 키패드 구현",
             description: "키보드 입력이 필요한 상황에서 마우스로 클릭하여 입력하는 가상 키패드를 구현했습니다. 이를 통해 키로거로부터 사용자의 입력을 보호할 수 있습니다."
           },
           {
@@ -456,6 +462,8 @@ export const PROJECTS: ProjectType[] = [
         ]
       }
     ],
-    etc: "첫 담당 프로젝트, 이론만 아는 상태로 구르면서 급성장 끼요옷",
+    etc:
+    "내가 입사하여 처음으로 맡았던 프로젝트였다.\n"+
+    "이론만 알던 초짜 개발자였던 나는 프로젝트 완성을 위해 신나게 구르면서 어엿한 개발자로 급성장 할 수 있었다.",
   }
 ];
