524b1dc325874c33df9a89f28424827760f5255d
sisu-01 <tesv010615@naver.com>
2026-09-07 14:01:28 +0900

    프로필

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -4,16 +4,6 @@ const Profile = () => {
   return (
     <section className={styles.hero}>
       <div className={styles.container}>
-        {/* 텍스트 영역 */}
-        <div className={styles.textContent}>
-          <h2 className={styles.title}>
-            안녕하세요, <br className={styles.mobileBr} />개발자입니다.
-          </h2>
-          <p className={styles.description}>
-            자기소개자기소개자기소
-          </p>
-        </div>
-        
         {/* 이미지 영역 */}
         <div className={styles.imageContent}>
           <div className={styles.imageWrapper}>
@@ -23,6 +13,17 @@ const Profile = () => {
               className={styles.profileImage} 
             />
           </div>
+        </div>
+        {/* 텍스트 영역 */}
+        <div className={styles.textContent}>
+          <h1 className={styles.title}>
+            개발자 <span className={styles.name}>이종원</span>의 <br className={styles.mobileBr} />포트폴리오
+          </h1>
+          <p className={styles.description}>
+            <h2>
+              안녕하십니까, 아이디어를 실체화하는 개발자 이종원입니다.
+            </h2>
+          </p>
         </div>
       </div>
     </section>
